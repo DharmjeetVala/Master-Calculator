823c8eb0444730824b04b21fda0a75c846d036aa
DharmjeetVala <dharmjeetvala@gmail.com>
2024-07-11 23:07:59 +0530

    Ui (Scientific Calc)

---
+++ app/src/main/java/com/example/master_calc/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.master_calc
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        findViewById<AppCompatButton>(R.id.simpleCalc).setOnClickListener {
+            val intent = Intent(this, Scientific_Calculator::class.java)
+            startActivity(intent)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/master_calc/Scientific_Calculator.kt
@@ -0,0 +1,15 @@
+package com.example.master_calc
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Scientific_Calculator : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_scientific_calculator)
+
+    }
+}
